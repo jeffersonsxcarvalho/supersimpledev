4dd42426aa2009f1a6c24dad2227a83f84c656d6
Jefferson Silva Ximenes de Carvalho <jeffersonsicarvalho@gmail.com>
2024-07-09 07:23:26 -0300

    done exercise 10i

--- JavaScript/exercises/exercise10/exercises10.js
+++ JavaScript/exercises/exercise10/exercises10.js
@@ -88,3 +88,51 @@ function toggle3(btnx) {
 	}
 }
 
+
+//10g - Copy of the 10-dom-project
+
+  /*String(25)
+      console.log('25' - 5);
+      console.log('25' + 5);
+
+      window.document
+      window.console.log('window');
+      window.alert*/
+
+      function handleCostKeydown(event) {
+        if (event.key === 'Enter') {
+          calculateTotal();
+        }
+      }
+
+      function calculateTotal() {
+        const inputElement = document.querySelector('.js-cost-input');
+        const msg = document.querySelector('.js-total-cost')
+        msg.style.color = 'black';
+        let cost = Number(inputElement.value);
+
+        if(cost < 0) {
+        	//msg.innerHTML.style.color = 'red';
+        	cost = 'Error: cost cannot be less than $0';
+        	msg.style.color = 'red';
+        }else if (cost < 40) {
+          cost = cost + 10;
+        }else{
+        	cost = cost;
+        }
+        
+          msg.innerHTML = `${cost}`;
+      }
+
+      function subscribe() {
+        const buttonElement = document.querySelector('.js-subscribe-button');
+
+        if (buttonElement.innerText === 'Subscribe') {
+          buttonElement.innerHTML = 'Subscribed';
+          buttonElement.classList.add('is-subscribed');
+        } else {
+          buttonElement.innerHTML = 'Subscribe';
+          buttonElement.classList.remove('is-subscribed');
+        }
+      }
+
