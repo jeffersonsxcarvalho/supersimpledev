0d76e40dc569bb0a0959c17de86be0e2156612ec
Jefferson S X Carvalho <105809116+jeffersonsxcarvalho@users.noreply.github.com>
2024-10-13 20:50:48 -0300

    Tests - Order summary
    Order summary test page is changing the real cart. I have to fix that.

--- JavaScript/exercises/exercise16/16-class/scripts/checkout/orderSummary.js
+++ JavaScript/exercises/exercise16/16-class/scripts/checkout/orderSummary.js
@@ -43,7 +43,9 @@ export function renderOrderSummary() {
 		
 
 		cartSummaryHTML +=	`
-		  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+		  <div class="cart-item-container
+		  	js-cart-item-container
+		  	js-cart-item-container-${matchingProduct.id}">
 	        <div class="delivery-date">
 	          Delivery date: ${calculateDeliveryDate(deliveryOption)}
 	        </div>
@@ -59,7 +61,7 @@ export function renderOrderSummary() {
 	            <div class="product-price">
 	              $${formatCurrency(matchingProduct.priceCents)}
 	            </div>
-	            <div class="product-quantity">
+	            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
 	              <span>
 	                Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantityValue}</span>
 	              </span>
